bc7035884a9e6055064585d3413b93a49f87940d
caixian <qq52577517@163.com>
2021-08-22 23:15:38 +0800

    re-format

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -15,46 +15,43 @@ class BaseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
-    public function authorize()
+    public function authorize ()
     {
         return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
      * @return array
      */
-    public function rules()
+    public function rules ()
     {
         return [
-            'name' => ['required', 'string', 'unique:people'],
-            'sex' => ['required', new SexType()],
-            'mobile' => ['required', 'numeric', new Mobile(), 'unique:people'],
-            'email' => ['required', 'string', 'email', 'unique:people'],
-            'real_name' => ['required', 'string', 'nullable'],
-            'password' => ['required', 'string', new Password()],
+            'name'             => ['required', 'string', 'unique:people'],
+            'sex'              => ['required', new SexType()],
+            'mobile'           => ['required', 'numeric', new Mobile(), 'unique:people'],
+            'email'            => ['required', 'string', 'email', 'unique:people'],
+            'real_name'        => ['required', 'string', 'nullable'],
+            'password'         => ['required', 'string', new Password()],
             'confirm_password' => ['required', 'string', 'same:password']
         ];
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
      * @return array
      */
-    public function messages()
+    public function messages ()
     {
         return [
-            'name' => '',
-            'sex' => ['required', new SexType()],
-            'mobile' => ['required', 'numeric', new Mobile(), 'unique:people'],
-            'email' => ['required', 'string', 'email', 'unique:people'],
-            'real_name' => ['required', 'string', 'nullable'],
-            'password' => ['required', 'string', new Password()],
+            'name'             => '',
+            'sex'              => ['required', new SexType()],
+            'mobile'           => ['required', 'numeric', new Mobile(), 'unique:people'],
+            'email'            => ['required', 'string', 'email', 'unique:people'],
+            'real_name'        => ['required', 'string', 'nullable'],
+            'password'         => ['required', 'string', new Password()],
             'confirm_password' => ['required', 'string', 'same:password']
         ];
     }
@@ -64,11 +61,11 @@ class BaseRequest extends FormRequest
      * @param Request $request
      * @throws ParameterException
      */
-    public function goCheck(Request $request)
+    public function goCheck (Request $request)
     {
         $validator = Validator::make($request->all(), self::rules());
 
-        throw new TokenException();
+//        throw new TokenException();
 
         $validator->after(function ($validator) {
             if ($validator->fails()) {
